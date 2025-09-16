5a778a4017ee34b6fd2af271b7942b75fc68b097
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-09-17 00:29:22 +0300

    feat: add CartBadge component with dynamic item count from Redux

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,23 +1,34 @@
 import { AppBar, Box, Container, List, ListItem, Toolbar } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import CartBadge from './common/CartBadge';
+import { useAppSelector } from '../redux/store';
+import { cartSelectors } from '../redux/cart/selectors';
 
 const Header = () => {
+  const cartItems = useAppSelector(cartSelectors.selectAll);
+  const countCartItems = cartItems.length;
   return (
     <AppBar position="sticky">
       <Container>
-        <Toolbar>
-          <Box component={'nav'} aria-label="Main navigation">
-            <List sx={{ display: 'flex', flexDirection: 'row' }}>
-              <ListItem>
-                <NavLink to={'/'}>Home</NavLink>
-              </ListItem>
-              <ListItem>
-                <NavLink to={'/products'}>Catalog</NavLink>
-              </ListItem>
-              <ListItem>
-                <NavLink to={'/cart'}>Cart</NavLink>
-              </ListItem>
-            </List>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            position: 'relative',
+          }}
+        >
+          <List sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+            <ListItem>
+              <NavLink to={'/'}>Home</NavLink>
+            </ListItem>
+            <ListItem>
+              <NavLink to={'/products'}>Catalog</NavLink>
+            </ListItem>
+          </List>
+
+          {/* Корзина справа */}
+          <Box sx={{ position: 'absolute', right: 0 }}>
+            <CartBadge count={countCartItems} />
           </Box>
         </Toolbar>
       </Container>
